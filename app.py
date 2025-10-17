8fd55c4586749810a4a3405d2eed411f1a6ad0c3
Aadarsha <aadarshakarkee0@gmail.com>
2025-10-17 15:38:58 +0545

    Feat: Added two blog html file created slug route for all the blog detail page fixed the company carousel in landing page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template_string
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
@@ -21,6 +21,29 @@ def serve_blog():
 def serve_contact():
     return send_from_directory('.', 'contact-us.html')
 
+@app.route('/blog/how-ai-tools-are-reshaping-business-strategies')
+def serve_blog_b1():
+    return send_from_directory('.', 'b1.html')
+
+@app.route('/blog/exploring-the-future-of-automation-and-intelligence')
+def serve_blog_b2():
+    return send_from_directory('.', 'b2.html')
+
+@app.route('/blog/a-comprehensive-guide-for-beginners-in-intelligence')
+def serve_blog_b3():
+    return send_from_directory('.', 'b3.html')
+
+@app.route('/blog/cybersecurity-and-quantum-computing')
+def serve_blog_b4():
+    return send_from_directory('.', 'b4.html')
+
+@app.route('/blog/empowering-global-defense-with-ai')
+def serve_blog_b5():
+    return send_from_directory('.', 'b5.html')
+
+@app.route('/blog/the-rise-of-augmented-intelligence-human-machine-collaboration')
+def serve_blog_b6():
+    return send_from_directory('.', 'b6.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
